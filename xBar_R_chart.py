655aadb373892e011449fbb7d899bfb7f7b429c6
Bülent Koçer <31631846+bulent2kocer@users.noreply.github.com>
2022-05-15 22:23:24 +0300

    Add files via upload

---
+++ xBar_R_chart.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[128]:
+
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+n=[2,3,4,5,6,7,8,9,10,11,12]
+A2=pd.Series([1.88,1.023,0.729,0.577,0.483,0.419,0.373,0.337,0.308,0.285,0.266],index=n)
+D3=pd.Series([0,0,0,0,0,0.076,0.136,0.184,0.223,0.256,0.283],index=n)
+D4=pd.Series([3.267,2.574,2.282,2.114,2.004,1.924,1.864,1.816,1.777,1.744,1.717],index=n)
+
+factors = pd.DataFrame(index=n, data={'A2': A2, 'D3': D3, 'D4':D4})
+
+
+# In[129]:
+
+
+def diff(x):
+    maxx=np.max(x,1)
+    minn=np.min(x,1)
+    return maxx-minn
+
+def x_bar_R_chart(x):
+    nn=x.shape[1]
+    x_bar=np.mean(xx,1)
+    x_bar_bar = np.mean(xx)
+    R = diff(x)
+    R_bar = np.mean(diff(x))
+    data_x={
+        'X_vals': x_bar,
+        'UCL_x': x_bar_bar + (R_bar * factors.iloc[nn]['A2']),
+        'CEN_x': x_bar_bar ,
+        'LCL_x': x_bar_bar - (R_bar * factors.iloc[nn]['A2']),
+        'R_vals': R,
+        'UCL_R': R_bar * factors.iloc[nn]['D4'],
+        'CEN_R': R_bar ,
+        'LCL_R': R_bar * factors.iloc[nn]['D3']}
+    result = pd.DataFrame(index=range(30),data=data_x)
+    return result
+
+
+# In[130]:
+
+
+result = x_bar_R_chart(np.random.randn(30,5))
+
+
+# In[131]:
+
+
+plt.plot(result['X_vals'],color='blue')
+plt.plot(result['UCL_x'],color='red')
+plt.plot(result['CEN_x'],color='green')
+plt.plot(result['LCL_x'],color='red')
+
+
+# In[132]:
+
+
+plt.plot(result['R_vals'],color='blue')
+plt.plot(result['UCL_R'],color='red')
+plt.plot(result['CEN_R'],color='green')
+plt.plot(result['LCL_R'],color='red')
+
